ae00bbd12a5e140f3a1347cd086ca2634f919a20
krivax <krivahz@gmail.com>
2019-11-10 09:26:52 -0300

    Todos os 3 desafios do 049

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/index.css';
 import Sobre from './object/Sobre';
 import Logo from './object/Logo';
+import Menu from './object/Menu';
 
 function App() {
   return (
     <div>
     <Logo />
     <Sobre />
+    <Menu />
     </div>
   );
 }
---
+++ src/object/Menu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+
+const Menu = ()=> {
+
+    return(<div className="menu">
+        <span className="menuHamburger" ></span>
+        <span className="menuHamburger" ></span>
+        <span className="menuHamburger" ></span>
+        </div>)
+
+}
+
+export default Menu;
